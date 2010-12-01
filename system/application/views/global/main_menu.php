f1661774f4a42b71ab3e9552eae3f9ae9c33eba4
Mat Sadler <mat@redstudio.co.uk>
2010-12-01 11:41:34 +0000

    made main menu a list

--- system/application/views/global/main_menu.php
+++ system/application/views/global/main_menu.php
@@ -1,53 +1,15 @@
-<div id="" class="menu_87">
-<a href="<?=base_url()?>">home</a>
-</div>
-
-
-
-<div id="" class="menu_87">
-<a href="<?=base_url()?>welcome/main/overview">overview</a>
-</div>
-
-
-
-<div id="" class="menu_87">
-<a href="<?=base_url()?>welcome/main/process">the process</a>
-</div>
-
-
-<a href="<?=base_url()?>welcome/main/signage">
-<div id="" class="menu_87">
-signage
-</div>
-</a>
-
-
-<div id="" class="menu_87">
-<a href="<?=base_url()?>welcome/main/exhibitions">exhibitions</a>
-</div>
-
-
-
-<div id="" class="menu_177">
-<a href="<?=base_url()?>welcome/main/professional">professional standards</a>
-</div>
-
-<div id="" class="menu_87">
-<a href="<?=base_url()?>news">news</a>
-</div>
-
-
-<div id="" class="menu_87">
-<a href="<?=base_url()?>contact">contact</a>
-</div>
+<ul id="topnav">
+    <li class="menu_87"><a href="<?=base_url()?>">home</a></li>
+    <li class="menu_87"><a href="<?=base_url()?>welcome/main/overview">overview</a></li>
+    <li class="menu_87"><a href="<?=base_url()?>welcome/main/process">the process</a></li>
+    <li class="menu_87"><a href="<?=base_url()?>welcome/main/signage">signage</a></li>
+    <li class="menu_87"><a href="<?=base_url()?>welcome/main/exhibitions">exhibitions</a></li>
+    <li class="menu_177"><a href="<?=base_url()?>welcome/main/professional">professional standards</a></li>
+     <li class="menu_87"><a href="<?=base_url()?>news">news</a></li>
+      <li class="menu_87"><a href="<?=base_url()?>contact">contact</a></li>
+</ul>
 
 
 
 
-<div id="" class="menu_87">
-&nbsp;
-</div>
 
-<div id="" class="menu_87">
-&nbsp;
-</div>
